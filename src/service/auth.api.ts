cef6e83a8b2fbc792564117add23eaf0b3e5f0e8
Felipe Lima <felip.3lima@hotmail.com>
2025-05-02 19:15:58 -0300

    feat: some fix

--- src/service/auth.api.ts
+++ src/service/auth.api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiWithoutHeader = axios.create({
-  baseURL: `http://localhost:8080`,
-  // baseURL: 'https://api-core.up.railway.app',
+  // baseURL: `http://localhost:8080`,
+  baseURL: 'https://api-core.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
